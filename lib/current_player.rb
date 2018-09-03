db3931bc3a179ef826e877cb01ed422e347c2e8a
Saint-Jean Germila <germila@noukod.com>
2018-09-03 19:01:35 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
 def turn_count(board)
   board.each do |index|
-  counte = 0
+  counter = 0
   if index == "X" || index == "O"
     counter += 1
   end
 
-#def current_player(game_board)
-#end
+def current_player(board)
+  
+end
