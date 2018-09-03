7459436f7cbe34cd94281f9b253570db218327ee
Saint-Jean Germila <germila@noukod.com>
2018-09-03 19:50:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,7 +5,10 @@ def turn_count(board)
     counter += 1
   end
 end
+return counter
+end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0? "X" : "O"
+   turn_count(board) % 2 == 0? "X" : "O"
+    
   end
